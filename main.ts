bffc74993afbab20ef2e19e89301b75d783d249d
Technoblade1231 <92799244+Technoblade1231@users.noreply.github.com>
2021-10-29 09:33:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,155 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(5)
+    basic.pause(100)
+    basic.showNumber(4)
+    basic.pause(100)
+    basic.showNumber(3)
+    basic.pause(100)
+    basic.showNumber(2)
+    basic.pause(100)
+    basic.showNumber(1)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # . .
+        # # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        # # # # .
+        . # # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . # # # #
+        . . # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . # # # #
+        . . # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # #
+        . . # # #
+        . . . # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . # # # #
+        . . # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . # # # #
+        . . # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # #
+        . . # # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.showString("Touchdown")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(5)
+    basic.pause(100)
+    basic.showNumber(4)
+    basic.pause(100)
+    basic.showNumber(3)
+    basic.pause(100)
+    basic.showNumber(2)
+    basic.pause(100)
+    basic.showNumber(1)
+    basic.pause(100)
+    basic.showNumber(0)
+})
+basic.forever(function () {
+	
+})
